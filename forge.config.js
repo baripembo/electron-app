8cd33555b97db69033a544d74098bcc7bbada110
baripembo <erikawei@gmail.com>
2023-08-23 16:21:26 -0400

    update

--- forge.config.js
+++ forge.config.js
@@ -27,4 +27,17 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'baripembo',
+          name: 'electron-app'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
